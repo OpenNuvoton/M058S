138c17db6867f9dcfcb65d7dae69809dd0eac7e1
MS20 YCChen5 <YCCHEN5@nuvoton.com>
2023-03-17 13:21:06 +0800

    Add SYS_PowerDown_MinCurrent sample code.
    Change-Id: Ic57f4d9f90dc5a29c179da11c4936220d2ee3215

--- Library/StdDriver/src/uart.c
+++ Library/StdDriver/src/uart.c
@@ -130,7 +130,7 @@ void UART_DisableInt(UART_T*  uart, uint32_t u32InterruptFlag)
  *
  *    @return       None
  *
- *    @details      The function is used to Enable UART auto flow control.
+ *    @details      The function is used to enable UART auto flow control.
  */
 void UART_EnableFlowCtrl(UART_T* uart)
 {
@@ -248,7 +248,7 @@ uint32_t UART_Read(UART_T* uart, uint8_t *pu8RxBuf, uint32_t u32ReadBytes)
         {
             u32delayno++;
             if(u32delayno >= 0x40000000)
-                return FALSE;
+                return u32Count;
         }
         pu8RxBuf[u32Count] = uart->RBR;    /* Get Data from UART RX  */
     }
@@ -262,7 +262,7 @@ uint32_t UART_Read(UART_T* uart, uint8_t *pu8RxBuf, uint32_t u32ReadBytes)
  *    @brief        Set UART line configuration
  *
  *    @param[in]    uart            The pointer of the specified UART module.
- *    @param[in]    u32baudrate     The register value of baudrate of UART module.
+ *    @param[in]    u32baudrate     The baudrate of UART module.
  *                                  If u32baudrate = 0, UART baudrate will not change.
  *    @param[in]    u32data_width   The data length of UART module.
  *                                  - UART_WORD_LEN_5
@@ -458,11 +458,11 @@ uint32_t UART_Write(UART_T* uart, uint8_t *pu8TxBuf, uint32_t u32WriteBytes)
     for(u32Count = 0; u32Count != u32WriteBytes; u32Count++)
     {
         u32delayno = 0;
-        while((uart->FSR & UART_FSR_TE_FLAG_Msk) == 0)  /* Wait Tx empty and Time-out manner */
+        while(uart->FSR & UART_FSR_TX_FULL_Msk) == 0)  /* Wait Tx not full or Time-out manner */
         {
             u32delayno++;
             if(u32delayno >= 0x40000000)
-                return FALSE;
+                return u32Count;
         }
         uart->THR = pu8TxBuf[u32Count];    /* Send UART Data from buffer */
     }
---
+++ SampleCode/RegBased/SYS_PowerDown_MinCurrent/KEIL/obj/startup_m058s.d
@@ -0,0 +1,1 @@
+.\obj\startup_m058s.o: ..\..\..\..\Library\Device\Nuvoton\M058S\Source\ARM\startup_M058S.s
